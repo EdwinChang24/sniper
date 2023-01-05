a6f2777ff7c2e35aa5bedebb3602a4a9e10c1a32
Edwin Chang <edwinchang2024@gmail.com>
2023-01-04 20:32:38 -0800

    Add quotes

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,3 +1,3 @@
 if not exist "build\" mkdir build\
-cmake -S . -B  build\ -DCMAKE_TOOLCHAIN_FILE=C:\\Program Files\vcpkg\scripts\buildsystems\vcpkg.cmake
-cmake --build build\ --config Release -DCMAKE_TOOLCHAIN_FILE=C:\\Program Files\vcpkg\scripts\buildsystems\vcpkg.cmake
+cmake -S . -B  build\ -DCMAKE_TOOLCHAIN_FILE="C:\\Program Files\vcpkg\scripts\buildsystems\vcpkg.cmake"
+cmake --build build\ --config Release -DCMAKE_TOOLCHAIN_FILE="C:\\Program Files\vcpkg\scripts\buildsystems\vcpkg.cmake"
